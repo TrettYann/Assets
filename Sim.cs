bf12987e1cd75c8e871cfd8be540d7501fda5a02
Yannik Tretter <tretter.yannik@icloud.com>
2025-06-12 16:19:37 +0200

    Fused Shaders
    Combined added the decay Shader to the main script. Implemented frame cap to compensate for the fast decay.

--- Sim.cs
+++ Sim.cs
@@ -11,14 +11,17 @@ public class Sim: MonoBehaviour
     }
 
     public ComputeShader computeShader;
+    public ComputeShader decayShader;
     public RenderTexture renderTexture;
     [Range(0, 1000)] public int agentCount;
     public int width = 256;
     public int height = 256;
     [Range(0f, 10f)] public float speed = 0.2f;
+    [Range(-1f, 1f)] public float decayFactor;
 
     ComputeBuffer computeBuffer;
-    int kernelHandle;
+    int mainKernel;
+    int decayKernel;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -27,7 +30,7 @@ public class Sim: MonoBehaviour
         createBuffer();
         InitAgents();
         setupShader();
-
+        Application.targetFrameRate = 60;
     }
 
 
@@ -38,21 +41,13 @@ public class Sim: MonoBehaviour
 
     private void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
-        int threadsPerGroup = 256;
-        int groups = Mathf.CeilToInt(agentCount / (float)threadsPerGroup);
+        
         if (renderTexture == null)
         {
             createTexture();
         }
 
-        computeShader.SetTexture(0, "Result", renderTexture);
-        computeShader.SetFloat("deltaTime", Time.deltaTime);
-        computeShader.SetInt("width", width);
-        computeShader.SetInt("height", height);
-        computeShader.SetFloat("deltaTime", Time.deltaTime);
-        computeShader.SetFloat("speed", speed);
-        computeShader.SetBuffer(0, "agents", computeBuffer);
-        computeShader.Dispatch(0, groups, 1, 1);
+        handleShader();
         Graphics.Blit(renderTexture, dest);
 
     }
@@ -63,6 +58,23 @@ public class Sim: MonoBehaviour
         renderTexture.enableRandomWrite = true;
         renderTexture.filterMode = FilterMode.Point;
         renderTexture.Create();
+    }
+
+    void handleShader()
+    {
+        // Main Shader
+        int threadsPerGroup = 256;
+        int groups = Mathf.CeilToInt(agentCount / (float)threadsPerGroup);
+        computeShader.SetTexture(mainKernel, "Result", renderTexture);
+        computeShader.SetFloat("deltaTime", Time.deltaTime);
+        computeShader.SetFloat("speed", speed);
+        computeShader.SetBuffer(mainKernel, "agents", computeBuffer);
+        computeShader.Dispatch(mainKernel, groups, 1, 1);
+
+        // Decay Shader
+        decayShader.SetFloat("decayFactor", decayFactor);
+        decayShader.SetTexture(decayKernel, "Result", renderTexture);
+        decayShader.Dispatch(mainKernel, width / 8, height / 8, 1);
     }
 
 
@@ -88,9 +100,13 @@ public class Sim: MonoBehaviour
 
     void setupShader()
     {
-        kernelHandle = computeShader.FindKernel("CSMain");
-        computeShader.SetBuffer(0, "agents", computeBuffer);
-        computeShader.SetTexture(0, "Result", renderTexture);
+        mainKernel = computeShader.FindKernel("CSMain");
+        decayKernel = decayShader.FindKernel("CSDecay");
+        computeShader.SetBuffer(mainKernel, "agents", computeBuffer);
+        computeShader.SetTexture(mainKernel, "Result", renderTexture);
+        computeShader.SetInt("width", width);
+        computeShader.SetInt("height", height);
+        decayShader.SetTexture(decayKernel, "Result", renderTexture);
 
     }
 
